17d03af792415b889a6fee42d9348220fa04d681
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-16 16:06:47 -0230

    update

---
+++ src/RollADie.java
@@ -0,0 +1,16 @@
+import java.security.SecureRandom;
+
+/**
+ * This class rolls a 6 sided die 20 times using secureRandom
+ * @author--Zheng Wang
+ */
+public class RollADie {
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 1; i <= 20; i++) {
+            int roll = random.nextInt(6) + 1;
+            System.out.printf("%nOn the %d th try you rolled %d",i,roll);
+        }
+    }
+}
